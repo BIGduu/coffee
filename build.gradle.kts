5c3295f768df819d2e0c016a0f19ff676953ed48
bigduu <mugeng.du@qq.com>
2021-12-28 23:48:03 +0800

    add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,16 +26,32 @@ subprojects {
     }
 
     dependencies {
-        implementation(kotlin("stdlib"))
 
+        // netty
+        implementation("io.netty:netty-all:4.1.72.Final")
+
+        // logback
+        implementation("org.slf4j:slf4j-api:1.7.32")
+        implementation("ch.qos.logback:logback-core:1.2.10")
+        testImplementation("ch.qos.logback:logback-classic:1.2.10")
+
+        // jackson
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+        implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
+        implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.1")
+
+        // kotlin
+        implementation(kotlin("stdlib"))
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
         testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
     }
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
+            incremental = true
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "16"
+            jvmTarget = "17"
         }
     }
     tasks.withType<Test> {
